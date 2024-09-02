fe41b78ef5d648439cacff8850f6f921ef7ff34f
Nelson Zuluaga <nelson.b.zuluaga@gmail.com>
2024-09-02 11:52:55 -0400

    Finished adding class based Views and also added URL updates

--- watchmateProject/watchlist_app/api/views.py
+++ watchmateProject/watchlist_app/api/views.py
@@ -21,5 +21,28 @@ class MovieListAV(APIView): #Inheriting the APIView class
             return Response(serializer.errors)
     
 
+class MovieDetailAV(APIView):
 
+    def get(self, request, pk):
+        try:
+            movie = Movie.objects.get(pk=pk)
+        except Movie.DoesNotExist:
+            return Response({'error': 'Movie not found'}, status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = MovieSerializer(movie)
+        return Response(serializer.data)
+        
+    def put(self, request, pk):
+        movie = Movie.objects.get(pk=pk)
+        serializer = MovieSerializer(movie, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else: 
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
  
+    def delete(self, request, pk):
+        movie = Movie.objects.get(pk=pk)
+        movie.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
