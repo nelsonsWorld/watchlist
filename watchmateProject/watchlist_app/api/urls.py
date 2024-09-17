da1a83a0636f5b9cc7f411f24a69a980df143fb9
Nelson Zuluaga <nelson.b.zuluaga@gmail.com>
2024-09-17 13:15:32 -0400

    This is for Viewsets and Routers video, perform DIFF to see the difference

--- watchmateProject/watchlist_app/api/urls.py
+++ watchmateProject/watchlist_app/api/urls.py
@@ -1,12 +1,22 @@
 from django.urls import path, include
-from watchlist_app.api.views import WatchListAV, WatchListDetailAV, StreamPlatformAV, StreamPlatformDetailAV, ReviewList, ReviewDetail, ReviewCreate
+from watchlist_app.api.views import WatchListAV, WatchListDetailAV, StreamPlatformAV, StreamPlatformDetailAV, ReviewList, ReviewDetail, ReviewCreate, StreamPlatformVS
+from rest_framework.routers import DefaultRouter #import Default router first
+#Version 18, video Viewsets and Routers. Define router first
+# 
+
+router = DefaultRouter() 
+router.register('stream', StreamPlatformVS, basename='streamplatform')
+
+
 
 urlpatterns = [
     path('list/', WatchListAV.as_view(), name='movie-list'), #remember as_view is a function and you need to add the '()'
     path('<int:pk>', WatchListDetailAV.as_view(), name='movie-detail'),
 
-    path('stream/', StreamPlatformAV.as_view(), name='stream'),
-    path('stream/<int:pk>', StreamPlatformDetailAV.as_view(), name='stream-detail'),
+    path('', include(router.urls)),
+
+    # path('stream/', StreamPlatformAV.as_view(), name='stream'),
+    # path('stream/<int:pk>', StreamPlatformDetailAV.as_view(), name='stream-detail'),
     
     # path('review', ReviewList.as_view(), name= 'review-list'),
     # path('review/<int:pk>', ReviewDetail.as_view(), name='review-detail'),
--- watchmateProject/watchlist_app/api/views.py
+++ watchmateProject/watchlist_app/api/views.py
@@ -1,8 +1,11 @@
+from django.shortcuts import get_object_or_404
+
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework import mixins
 from rest_framework import generics
+from rest_framework import viewsets
 
 from watchlist_app.models import WatchList, StreamPlatform, Review
 from watchlist_app.api.serializers import WatchListSerializer, StreamPlatformSerializer, ReviewSerializer
@@ -51,7 +54,19 @@ class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
 #     def post(self, request, *args, **kwargs):
 #         return self.create(request, *args, **kwargs)
     
+class StreamPlatformVS(viewsets.ViewSet):
 
+    def list(self,request):
+    #type object 'StreamPlatform' has no attribute 'objects' is because you used the same name for class as model
+        queryset = StreamPlatform.objects.all()
+        serializer = StreamPlatformSerializer(queryset, many=True)
+        return Response(serializer.data)
+    
+    def retrieve(self, request, pk=None):
+        queryset = StreamPlatform.objects.all()
+        watchlist = get_object_or_404(queryset, pk=pk)
+        serializer = StreamPlatformSerializer(watchlist)
+        return Response(serializer.data)
 
 class StreamPlatformAV(APIView):
 
