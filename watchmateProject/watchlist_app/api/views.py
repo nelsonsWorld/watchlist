ee282d829912118043e40c41e1a8a01fd7368d73
Nelson Zuluaga <nelson.b.zuluaga@gmail.com>
2024-09-17 23:55:55 -0400

    stopped @ 6:05 in ModelViewSets video

--- watchmateProject/watchlist_app/api/views.py
+++ watchmateProject/watchlist_app/api/views.py
@@ -53,20 +53,38 @@ class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
 
 #     def post(self, request, *args, **kwargs):
 #         return self.create(request, *args, **kwargs)
-    
-class StreamPlatformVS(viewsets.ViewSet):
 
-    def list(self,request):
-    #type object 'StreamPlatform' has no attribute 'objects' is because you used the same name for class as model
-        queryset = StreamPlatform.objects.all()
-        serializer = StreamPlatformSerializer(queryset, many=True)
-        return Response(serializer.data)
+#Lets you create,get,post, and put depending on the application
+# class StreamPlatformVS(viewsets.ModelViewSet):
+#     queryset = StreamPlatform.objects.all()
+#     serializer_class = StreamPlatformSerializer
+
+class StreamPlatformVS(viewsets.ReadOnlyModelViewSet):
+    queryset = StreamPlatform.objects.all()
+    serializer_class = StreamPlatformSerializer
     
-    def retrieve(self, request, pk=None):
-        queryset = StreamPlatform.objects.all()
-        watchlist = get_object_or_404(queryset, pk=pk)
-        serializer = StreamPlatformSerializer(watchlist)
-        return Response(serializer.data)
+# class StreamPlatformVS(viewsets.ViewSet):
+
+#     def list(self,request):
+#     #type object 'StreamPlatform' has no attribute 'objects' is because you used the same name for class as model
+#         queryset = StreamPlatform.objects.all()
+#         serializer = StreamPlatformSerializer(queryset, many=True)
+#         return Response(serializer.data)
+    
+#     def retrieve(self, request, pk=None):
+#         queryset = StreamPlatform.objects.all()
+#         watchlist = get_object_or_404(queryset, pk=pk)
+#         serializer = StreamPlatformSerializer(watchlist)
+#         return Response(serializer.data)
+
+#     def create (self, request, *args, **kwargs):
+#         serializer = StreamPlatformSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         else:
+#             return Response(serializer.errors)
+        
 
 class StreamPlatformAV(APIView):
 
