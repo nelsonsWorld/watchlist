b901b88e41af9b3ab7db81026aff0f9c8c244dec
Nelson Zuluaga <nelson.b.zuluaga@gmail.com>
2024-09-18 23:08:33 -0400

    I got nothin'

--- watchmateProject/watchlist_app/models.py
+++ watchmateProject/watchlist_app/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.contrib.auth.models import User
+
+
 class StreamPlatform(models.Model):
     name = models.CharField(max_length=30)
     about = models.CharField(max_length=150)
