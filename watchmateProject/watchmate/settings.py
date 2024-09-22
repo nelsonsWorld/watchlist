25e22ffc45ad2c4dc01ae284d61ad09cb34f4c23
Nelson Zuluaga <nelson.b.zuluaga@gmail.com>
2024-09-22 15:07:54 -0400

    Global Throttle settings

--- watchmateProject/watchmate/settings.py
+++ watchmateProject/watchmate/settings.py
@@ -133,7 +133,15 @@ REST_FRAMEWORK = {
     # ],
     'DEFAULT_AUTHENTICATION_CLASSES':[
         'rest_framework.authentication.TokenAuthentication',
-    ]
+    ],
+    'DEFAULT_THROTTLE_CLASSES':[
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '1/day',
+        'user': '1000/day'
+    }
 }
 
 
