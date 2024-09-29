ae9e93c8bff60eb5f0e86357b3e717090eb590ba
Nelson Zuluaga <nelson.b.zuluaga@gmail.com>
2024-09-29 11:47:39 -0400

    added all the initial Pagination configurations

--- watchmateProject/watchlist_app/api/pagination.py
+++ watchmateProject/watchlist_app/api/pagination.py
@@ -1,4 +1,8 @@
 from rest_framework.pagination import PageNumberPagination
 
 class WatchListPagination(PageNumberPagination):
-    page_size= 10
+    page_size= 7
+    page_query_param = 'p' # Name of the string to identify page number. Default is page
+    page_size_query_param = 'size' #It allows client to dictate size of results, ex: http://127.0.0.1:8000/watch/list2/?size=10 also p=last default takes it to end page.
+    max_page_size = 50 #allows you to limit the "page_size_query_param" attribute
+    last_page_strings = 'end' #p=last default takes it to end page.
